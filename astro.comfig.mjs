ea4e8efa2253245884caf54bd0e1eaff79874868
Jacob <jacobalghamdi5@gmail.com>
2026-06-21 12:31:33 -0700

    Add Astro configuration file for site and base URL

---
+++ astro.comfig.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://jacobalghamdi5-sys.github.io',
+  base: '/GameScript-v1.0S-CL',
+});
